1266fbe5721e0109ec7affda2abc5788dd4987f5
Kaloyan Todorov <kaloyan.todorov@rwth-aachen.de>
2018-04-08 18:55:49 +0200

    The jobs directory is now created on application start if it doesn't exist

--- src/main/java/de/cmt/cometportable/CometPortableApplication.java
+++ src/main/java/de/cmt/cometportable/CometPortableApplication.java
@@ -5,10 +5,30 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
 public class CometPortableApplication {
+
+	@PostConstruct
+	private void init() {
+
+		Path path = Paths.get("jobs");
+
+		if(!Files.exists(path) || !Files.isDirectory(path)) {
+			try {
+				Files.createDirectory(path);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CometPortableApplication.class, args);
